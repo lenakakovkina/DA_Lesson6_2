6a18e74359116c42d6b4e876f62857561e180cf8
Samsung <elenakakovkina89@gmail.com>
2024-07-03 22:24:37 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int ValueToGuess = 109;
+Console.WriteLine("Game! Guess the X value and win! Try to input some value");
+int UserGuess = int.Parse(Console.ReadLine());
+while (UserGuess != ValueToGuess)
+{
+    if (UserGuess > ValueToGuess)
+    {
+        Console.WriteLine("No.Try one more time. The value should be less.");
+        UserGuess = int.Parse(Console.ReadLine());
+    }
+    else
+    {
+        Console.WriteLine("No.Try one more time. The value should be more.");
+        UserGuess = int.Parse(Console.ReadLine());
+    }
+}
+Console.WriteLine("Congratulations! You've guessed the correct value!");
